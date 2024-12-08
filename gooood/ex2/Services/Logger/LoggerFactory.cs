9e72758fedb8b1aded82368c379b09fff9e3c660
AyalaFlemen <38327931986@mby.co.il>
2024-12-08 17:45:38 +0200

    ‘initalChange’

---
+++ gooood/ex2/Services/Logger/LoggerFactory.cs
@@ -0,0 +1,30 @@
+﻿using ex2.Services.Logger;
+
+namespace ex2.Services.Logger
+{
+    public class LoggerFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public LoggerFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ILoggerService GetLogger(string useFileLogger)
+        {
+            if (useFileLogger == "console")
+            {
+                return _serviceProvider.GetRequiredService<FileLoggerService>();
+            }
+            if (useFileLogger == "file")
+            {
+                return _serviceProvider.GetRequiredService<ConsoleLoggerService>();
+            }
+            else 
+            {
+                return _serviceProvider.GetRequiredService<LoggerData>();
+            }
+        }
+    }
+}
